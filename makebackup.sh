0480bbe84056694fc1e620500212a17a1a3948f0
@RubenKelevra <cyrond@gmail.com>
2020-04-18 07:00:48 +0200

    update readme

--- makebackup.sh
+++ makebackup.sh
@@ -8,8 +8,8 @@
 #
 ########
 
-# simple script to backup a arch linux system, while avoiding to store any file
-# supplied by the package manager - for efficient backups
+# simple script to backup arch linux systems, while avoiding to store any file
+# supplied by the package manager - for space efficient backups
 #
 # requires: 
 # - restic
