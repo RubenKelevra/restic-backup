dfff4841bdeac537ae47b1fc54ad40974b37c30a
@RubenKelevra <cyrond@gmail.com>
2020-04-18 07:08:52 +0200

    a bit more unique filenames (in tmp folder) to avoid collisions

--- makebackup.sh
+++ makebackup.sh
@@ -37,18 +37,18 @@ echo "Generating exclude lists..."
 # fetch all files currently supplied by packages
 rm -f /tmp/pkg_files
 while IFS= read -r -d $'\n' filepath; do
-	[ -f "$filepath" ] && echo "$filepath" >> /tmp/pkg_files
+	[ -f "$filepath" ] && echo "$filepath" >> /tmp/restic-backup.pkg_files
 done < <(sudo pacman -Ql | cut -f 1 -d ' ' --complement)
 
 # check all files supplied by packages for changes, and write the changed files to a list
-sudo paccheck --md5sum --quiet --db-files --noupgrade --backup | awk '{ print $2 }' | sed "s/'//g" > /tmp/changed_files
+sudo paccheck --md5sum --quiet --db-files --noupgrade --backup | awk '{ print $2 }' | sed "s/'//g" > /tmp/restic-backup.changed_files
 
 # backup the changed files (remove them from the blacklist)
-grep -v -x -f /tmp/changed_files /tmp/pkg_files | sed 's/\[/\\[/g' > /tmp/blacklist
+grep -v -x -f /tmp/restic-backup.changed_files /tmp/restic-backup.pkg_files | sed 's/\[/\\[/g' > /tmp/restic-backup.blacklist
 
 # add the global exclude list to the black list
-cat ~/makebackup.excludes >> /tmp/blacklist
-echo "$CACHEDIR" >> /tmp/blacklist
+cat ~/makebackup.excludes >> /tmp/restic-backup.blacklist
+echo "$CACHEDIR" >> /tmp/restic-backup.blacklist
 
 echo "Generating package lists..."
 
@@ -57,15 +57,15 @@ sudo pacman -Qne | sudo tee /.explicit_packages.list >/dev/null
 sudo pacman -Qme | sudo tee /.explicit_foreign_packages.list >/dev/null
 
 echo "Backing up..."
-restic -r sftp:$server:/home/$user/backups/$hostname --verbose --cache-dir="$cachdir" backup / --exclude-file=/tmp/blacklist
+restic -r sftp:$server:/home/$user/backups/$hostname --verbose --cache-dir="$cachdir" backup / --exclude-file=/tmp/restic-backup.blacklist
 
 echo "Full system-backup done. Forgetting old snapshots..."
 restic -r sftp:$server:/home/$user/backups/$hostname --cache-dir="$cachdir" forget --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
 
 sudo setcap cap_dac_read_search=-ep /usr/bin/restic
 
-rm -f /tmp/pkg_files 2>/dev/null || true
-rm -f /tmp/changed_files 2>/dev/null || true
-rm -f /tmp/blacklist 2>/dev/null || true
+rm -f /tmp/restic-backup.pkg_files 2>/dev/null || true
+rm -f /tmp/restic-backup.changed_files 2>/dev/null || true
+rm -f /tmp/restic-backup.blacklist 2>/dev/null || true
 
 echo "Operation completed."
